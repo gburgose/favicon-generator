ed466996b17d1e33025c7531b7c878995e0954f5
Gabriel Burgos <hola@gabrielburgos.cl>
2025-07-26 14:17:55 -0400

    feat: Add LogRocket for improved error tracking and analytics
    - Integrated LogRocket version 10.1.0 into the project for enhanced monitoring of user sessions and error tracking.
    - Updated package.json, package-lock.json, and yarn.lock to include LogRocket dependency.
    - Imported LogRocket in layout.tsx to initialize tracking.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import CookieConsent from "@/components/CookieConsent";
 import ServiceWorkerRegistration from "@/components/ServiceWorkerRegistration";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import "@/utils/logrocket";
 
 const montserrat = Montserrat({
   subsets: ['latin'],
